d25b90c98c9a7d2b5e055d2a93229b52df62ae58
Devin Knight <knightdevin@hotmail.com>
2020-05-08 11:28:58 -0400

    Update index.js
    Group changes to associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,13 +2,15 @@ const User = require('./user')
 const Location = require('./location')
 const LocationReview = require('./locationReview')
 const Community = require('./community')
-const CommunityMod = require('./communityMod')
+const CommunityMods = require('./communityMod')
 const CommunitySubs = require('./communitySubs')
-const Photos = require('./photos')
+const Photo = require('./photos')
 const PostComment = require('./postComment')
 const UserFollowers = require('./userFollowers')
 const UserPost = require('./userPost')
 
+
+// =========== ASSOCIATIONS BELOW ===========
 User.hasMany(UserPost)
 UserPost.belongsTo(User)
 UserPost.hasMany(PostComment)
@@ -25,28 +27,17 @@ Photo.belongsTo(UserPost)
 Photo.belongsTo(Location)
 Photo.belongsTo(LocationReview)
 Photo.belongsTo(PostComment)
-User.belongsToMany(User, {as: 'Followers', through: 'UserFollowers'})
-Community.belongsToMany(User, {through: 'CommunitySubs'})
-User.belongsToMany(Community, {as: 'Subscribers', through: 'CommunitySubs'})
-// User.belongsToMany(Community, { as : "Moderator", through: "CommunityModerator"})
+User.belongsToMany(User, {as: "Followers", through: "UserFollowers"})
+Community.belongsToMany(User, {through: "CommunitySubs"})
+User.belongsToMany(Community, {as: "Subscribers", through: "CommunitySubs"})
+User.belongsToMany(Community, { as : "Moderator", through: "CommunityMods"})
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
+// =========== Exports Below ===========
 module.exports = {
   User,
   Community,
-  CommunityMod,
+  CommunityMods,
   CommunitySubs,
   Location,
   LocationReview,
