61b1445a14c1a570951927a3039fc8feccff2cfd
francomt <50552297+francomt@users.noreply.github.com>
2020-05-11 11:12:13 -0400

    cleaned up commented code

--- client/components/Map.js
+++ client/components/Map.js
@@ -1,9 +1,7 @@
 import React from 'react'
-// const mapboxgl = require('mapbox-gl')
 import MapGL, {GeolocateControl} from 'react-map-gl'
 
 import {mapboxToken} from '../../secrets'
-// mapboxgl.accessToken = mapboxToken
 
 export class Map extends React.Component {
   constructor() {
@@ -17,20 +15,9 @@ export class Map extends React.Component {
         longitude: -73.935242,
         zoom: 8
       }
-      //40.7128° N, 74.0060° W
     }
   }
-  componentDidMount() {
-    // const map = new mapboxgl.Map({
-    //   container: 'map',
-    //   center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
-    //   zoom: 12, // starting zoom
-    //   style: 'mapbox://styles/mapbox/streets-v10' // mapbox has lots of different map styles available.
-    // })
-    // const cPark = new mapboxgl.Marker().setLngLat([40.7829, 73.9654]).addTo(map)
-    // var marker = new mapboxgl.Marker().setLngLat([30.5, 50.5]).addTo(map)
-    // const centralPark = new mapboxgl.LngLat([40.7829, 73.9654])
-  }
+  
   addMarker() {}
   render() {
     const geolocateStyle = {
@@ -38,13 +25,8 @@ export class Map extends React.Component {
       margin: '50px',
       padding: '10px'
     }
-    console.log('THIS IS STATE >>>>>', this.state)
-    // const _onViewportChange = viewport => this.setState.viewport({...viewport, transitionDuration: 3000 })
     return (
       <div>
-        {/* <MapGL {...this.state.viewport} mapboxApiAccessToken={mapboxToken}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-        onViewportChange={_onViewportChange}/> */}
         <MapGL
           {...this.state.viewport}
           mapStyle="mapbox://styles/mapbox/streets-v11"
