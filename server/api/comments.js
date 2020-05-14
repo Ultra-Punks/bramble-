62e0647d53a03d6a33a2084bda140201b06f840e
Devin Knight <knightdevin@hotmail.com>
2020-05-14 13:56:40 -0400

    drafted APIs & checked associations. All comments JSONS appearing

---
+++ server/api/comments.js
@@ -0,0 +1,24 @@
+const router = require('express').Router()
+const {PostComment} = require('../db/models')
+
+module.exports = router
+
+// get all comments
+router.get('/', async (req, res, next) => {
+  try {
+    const allComments = await PostComment.findAll()
+    res.json(allComments)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// // get single comment according to commentId
+// router.get('/:commentId', async (req, res, next) => {
+//   try {
+//     const singleComment = await PostComment.findByPk(req.params.commentId)
+//     res.json(singleComment)
+//   } catch (error) {
+//     next(error)
+//   }
+// })
