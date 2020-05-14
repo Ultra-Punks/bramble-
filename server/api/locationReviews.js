2235df5b062b493f344a155ed9215218295ec56a
Devin Knight <knightdevin@hotmail.com>
2020-05-14 09:24:11 -0400

    API routes drafted for review by location & review by user...not fully  functional

--- server/api/locationReviews.js
+++ server/api/locationReviews.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {LocationReview} = require('../db/models')
+const {LocationReview, Location, User} = require('../db/models')
 
 module.exports = router
 
@@ -18,6 +18,50 @@ router.get('/:reviewId', async (req, res, next) => {
   try {
     const singleReview = await LocationReview.findByPk(req.params.reviewId)
     res.json(singleReview)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// REVISIT...NOT WORKING CURRENTLY...
+// get all reviews of a specific location
+router.get('/of/:locationId', async (req, res, next) => {
+  try {
+    const locationReviews = await Location.findOne({
+      where: {
+        locationId: req.params.locationId
+      },
+      include: [{model: Location}]
+    })
+    res.json(locationReviews)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// REVISIT...NOT WORKING CURRENTLY...
+// get all reviews from a specific user
+router.get('/from/:username', async (req, res, next) => {
+  try {
+    const allUserReviews = await User.findOne({
+      where: {
+        username: req.params.username
+      },
+      include: [{model: LocationReview}]
+    })
+    res.json(allUserReviews.locationReviews)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// delete a review by Id
+router.delete('/:reviewId', async (req, res, next) => {
+  try {
+    const numOfDeleted = await LocationReview.destroy({
+      where: {id: req.params.reviewId}
+    })
+    res.status(200).json(numOfDeleted)
   } catch (error) {
     next(error)
   }
