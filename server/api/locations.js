a0c256892b70e859545a502ef10b1f5811f67ef9
Robu Waldorf <fieldassociationrecording@gmail.com>
2020-05-15 16:20:55 -0400

    localGeocoder sort of working

--- server/api/locations.js
+++ server/api/locations.js
@@ -5,9 +5,11 @@ module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
+    //below will only return locations with coordinates
     const locations = await Location.findAll({
       where: {point: {[Op.ne]: null}}
     })
+    // const locations = await Location.findAll()
     res.json(locations)
   } catch (err) {
     next(err)
