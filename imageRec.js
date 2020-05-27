43b37810beb8d1df33a9b4626e5d60b093e87c6a
Franco Trelles <franco.trellesm@gmail.com>
2020-05-27 00:59:23 -0400

    fix on uploading video preview

---
+++ imageRec.js
@@ -0,0 +1,34 @@
+const vision = require('@google-cloud/vision')
+
+async function scanner(image) {
+  let allLabels = []
+
+  const client = new vision.ImageAnnotatorClient({
+    keyFilename: ''
+  })
+  const [result] = await client.labelDetection(image)
+  const labels = result.labelAnnotations
+  labels.forEach(label => {
+    if (label.score > 0.65) {
+      allLabels.push(label.description)
+    }
+  })
+
+  const [textResults] = await client.textDetection(image)
+  const detections = textResults.textAnnotations
+  detections.forEach(text => {
+    allLabels.push(text.description)
+  })
+
+  const [logoResult] = await client.logoDetection(image)
+  const logos = logoResult.logoAnnotations
+  logos.forEach(logo => {
+    if (logo.score > 0.65) {
+      allLabels.push(logo.description)
+    }
+  })
+
+  return allLabels
+}
+
+module.exports = scanner
